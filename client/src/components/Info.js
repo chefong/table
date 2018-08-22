14eaa56c927a5b81a9a0ad9cf02f2f96ede55271
ericong18 <ericong18@gmail.com>
2018-08-21 21:36:37 -0700

    Added information to welcome page

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Info.css';
 
 const anonymous = require('../assets/imgs/anonymous.png');
+const tableExample = require('../assets/imgs/table-example.png');
 
 const Info = () => {
   return (
@@ -19,6 +20,36 @@ const Info = () => {
         <div className="col-md-4">
           <div className="anonymous-container">
             <img src={anonymous} alt="anonymous people" id="anonymous-people"/>
+          </div>
+        </div>
+      </div>
+      <div className="table-info-container">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className="table-desc-container">
+            	<p className="table-desc">
+                Express your problems using a table. Who knows, you might also be helping out someone too!
+              </p>
+              <p className="table-desc">
+                Click on the left and right arrows to see what others have said. Hopefully their answers make you feel at ease.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="table-container">
+            	<img src={tableExample} alt="Table" id="table-example"/>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="table-desc-container">
+            	<p className="table-desc">
+                Upvote someone's table to let them know that you agree or that you're there for them. At the end of the day,
+                we're all one big community.
+              </p>
+              <p className="table-desc">
+                Respond to someone by clicking on the comment icon. Your words can definitely mean a lot to everyone.
+              </p>
+            </div>
           </div>
         </div>
       </div>
